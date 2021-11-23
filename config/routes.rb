6fa0ec3dd5bc641cecd3750343792bbbc518d812
Ubuntu <ubuntu@ip-172-31-31-41.us-east-2.compute.internal>
2021-11-23 15:17:44 +0000

    Implement Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+ 
+  devise_for :members
   resources :users
   # get 'home/index'
   get 'home/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root to: "home#index"
 end
